d7af40352df784c2334f026dc6a1bb8bd105908a
Unknown <mjohnson030@west-mec.org>
2019-03-31 17:05:57 -0700

    Final Commit ( (Ready for Review)
    Updated Javascript and removed bugs as well as increased functionality. Added the updateCount function.

--- bw_review.js
+++ bw_review.js
@@ -31,27 +31,69 @@
    countCharacters(textStr)
       Returns the number of a non-whitespace characters
       within textStr
-
+ 
 */
 
-window.onload = init();
-
-init() {
-      var stars;
+window.onload = init;
+// The init function to define event listeners.
+function init() {
+      var stars = document.querySelectorAll("span#stars img");
       
+      for (var i = 0; i < stars.length; i++) {
+            stars[i].style.cursor = "pointer";
+            stars[i].addEventListener("mouseenter", lightStars);
+      }
+      document.getElementById("comment").addEventListener("keyup", updateCount);
+}
+
+function lightStars(e) {
+      var starNumber = e.target.alt;
+      var stars = document.querySelectorAll("span#stars img");
+      for (var i = 0; i < starNumber; i++) {
+            stars[i].src = "bw_star2.png";
+
+      }
+      for (var i = starNumber; i < 5; i++) {
+            stars[i].src = "bw_star.png";
+
+      }
+      document.getElementById("rating").value = starNumber + " star(s)";
+      stars[i].addEventListener("mouseleave", turnOffStars);
+
+      stars[i].addEventListener("click",
+            function () {
+                  document.removeEventListener("mouseleave", turnOffStars);
+
+            });
+}
+
+function turnOffStars(e) {
+      var stars = document.querySelectorAll("span#stars img");
+      for (var i = 0; i < stars; i++) {
+            stars[i].src = "bw_star.png";
+      }
+      document.getElementById("rating").value = "";
+}
+
+function updateCount() {
+      var commentText = document.getElementById("comment").value;
+      var charCount = countCharacters(commentText);
+      var wordCountBox = document.getElementById("wordCount");
+      wordCountBox.value = charCount + "/1000";
+      if (charCount > 1000) {
+            wordCountBox.style.color = "white";
+            wordCountBox.style.backgroundColor = "red";
+      } else {
+            wordCountBox.style.color = "black";
+            wordCountBox.style.backgroundColor = "white";
+      }
 }
 
 
-  
-
-
-  
-  
-  
 /*=================================================================*/
 
 function countCharacters(textStr) {
-   var commentregx = /\s/g;
-   var chars = textStr.replace(commentregx, "");
-   return chars.length;
-}   
+      var commentregx = /\s/g;
+      var chars = textStr.replace(commentregx, "");
+      return chars.length;
+}
